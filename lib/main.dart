1898cb58e433ba65d8cf2a6d25d5e6034da1ff47
aycanihan <aycanihandd@gmail.com>
2026-04-28 13:28:05 +0300

    firebase entegrasyonu

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'firebase_options.dart';
 
 // ── COLORS ──
 class AppColors {
@@ -136,10 +138,16 @@ final _router = GoRouter(
 // ── MAIN ──
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Brightness.light,
   ));
+
   runApp(const EtkinlikApp());
 }
 
